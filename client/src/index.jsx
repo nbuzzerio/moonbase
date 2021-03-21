53031bb6a9d51cc8c48c152993061bd0f3e2bb7e
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-03-21 14:32:51 -0400

    initial application setup

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+function App() {
+    return (
+        <article id="main">
+            <h1>LTO MOONBASE NODE: 98/2_MOONBASE</h1>
+        </article>
+    )
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('home')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            "presets": [
+              [
+                "@babel/preset-env",
+                {
+                  "targets": {
+                    "node": "current"
+                  }
+                }
+              ],
+              "@babel/preset-react"
+            ]
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+    ]
+  },
+}
