30bb38bf746b6bbc3b17ce2ff6cbbff3bebcb666
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-04-08 21:06:51 -0400

    refactored stats.jsx and organized helper functions in other folders

---
+++ client/src/helper/filterBlocksEarned.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+export default function filterBlocksEarned(state, dispatch, ACTIONS) {
+  let blocksEarned = [];
+  let timeZoneDif = 14400000;
+  if (state.barDataBool) {
+    const currentWeekDay = new Date(
+      Date.parse(state.nodeData[state.nodeData.length - 1].date) + timeZoneDif
+    ).getDay();
+
+    dispatch({
+      type: ACTIONS.SET_CURRENT_WEEK_DAY,
+      payload: {
+        currentWeekDay: currentWeekDay,
+      },
+    });
+
+    let weekEnd = Date.now() - 86400000 * (currentWeekDay + 1);
+    const weekStart = new Date(weekEnd - 518400000);
+    weekEnd = new Date(weekEnd);
+    dispatch({
+      type: ACTIONS.SET_WEEK_END,
+      payload: {
+        weekEnd: weekEnd,
+      },
+    });
+
+    dispatch({
+      type: ACTIONS.SET_WEEK_START,
+      payload: {
+        weekStart: weekStart,
+      },
+    });
+
+    let sunNodeData = state.nodeData.length - (currentWeekDay + 8);
+    let weekBlocksTotal = 0;
+    for (let i = sunNodeData; blocksEarned.length < 7; i++) {
+      let blocks = state.nodeData[i].blocks;
+      blocksEarned.push(blocks);
+      weekBlocksTotal += blocks;
+    }
+
+    dispatch({
+      type: ACTIONS.SET_WEEK_BLOCKS_TOTAL,
+      payload: {
+        weekBlocksTotal: weekBlocksTotal,
+      },
+    });
+
+    let weekNodeData = { ...state.weekBlocksChart.props.children.props.data };
+    weekNodeData.datasets[0].data = blocksEarned;
+    weekNodeData.datasets[0].label = "LTO Blocks Earned";
+
+    dispatch({
+      type: ACTIONS.SET_WEEK_BLOCKS_CHART,
+      payload: {
+        weekBlocksChart: (
+          <div className="chart">
+            <Bar
+              data={weekNodeData}
+              height={300}
+              width={500}
+              options={{
+                title: {
+                  display: true,
+                  text: `LTO Blocks Earned Week of: ${weekStart.getMonth() + 1}/${weekStart.getDate()}`,
+                  position: "bottom",
+                  fontColor: "black",
+                  fontSize: 30,
+                },
+                legendPositon: "bottom",
+                maintainAspectRatio: false,
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
+                },
+              }} />
+          </div>
+        ),
+      },
+    });
+
+    dispatch({
+      type: ACTIONS.SET_BAR_DATA_BOOL,
+      payload: { barDataBool: false },
+    });
+  }
+}
--- client/src/sections/Stats.jsx
+++ client/src/sections/Stats.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
-import getBlockData from "../_getBlockData";
-import getNodeData from "../_getNodeData";
+import React, { useEffect, useReducer } from "react";
+import getBlockData from "../apiRequests/_getBlockData";
+import getNodeData from "../apiRequests/_getNodeData";
 import { Bar, Line, defaults } from "react-chartjs-2";
+
+import filterBlocksEarned from "../helper/filterBlocksEarned.jsx";
 
 export default function Stats() {
   const ACTIONS = {
@@ -126,7 +128,7 @@ export default function Stats() {
               {state.weekStart.getDate()} - {state.weekEnd.getMonth() + 1}/
               {state.weekEnd.getDate()}
               <br></br>
-              LTO Moonbase verified 2165 transactions on the LTO Blockchain
+              LTO Moonbase verified {2165} transactions on the LTO Blockchain
             </p>
           </div>
         </aside>
@@ -138,108 +140,13 @@ export default function Stats() {
               {state.weekStart.getDate()} - {state.weekEnd.getMonth() + 1}/
               {state.weekEnd.getDate()}
               <br></br>
-              LTO Moonbase earned 503 LTO in transactions for its leasers
+              LTO Moonbase earned {503} LTO in transactions for its leasers
             </p>
           </div>
         </aside>
       </div>
     </div>
   );
-}
-
-function filterBlocksEarned(state, dispatch, ACTIONS) {
-  let blocksEarned = [];
-  let timeZoneDif = 14400000;
-  if (state.barDataBool) {
-    const currentWeekDay = new Date(
-      Date.parse(state.nodeData[state.nodeData.length - 1].date) + timeZoneDif
-    ).getDay();
-
-    dispatch({
-      type: ACTIONS.SET_CURRENT_WEEK_DAY,
-      payload: {
-        currentWeekDay: currentWeekDay,
-      },
-    });
-
-    let weekEnd = Date.now() - 86400000 * (currentWeekDay + 1);
-    const weekStart = new Date(weekEnd - 518400000);
-    weekEnd = new Date(weekEnd);
-    dispatch({
-      type: ACTIONS.SET_WEEK_END,
-      payload: {
-        weekEnd: weekEnd,
-      },
-    });
-
-    dispatch({
-      type: ACTIONS.SET_WEEK_START,
-      payload: {
-        weekStart: weekStart,
-      },
-    });
-
-    let sunNodeData = state.nodeData.length - (currentWeekDay + 8);
-    let weekBlocksTotal = 0
-    for (let i = sunNodeData; blocksEarned.length < 7; i++) {
-      let blocks = state.nodeData[i].blocks;
-      blocksEarned.push(blocks);
-      weekBlocksTotal+= blocks;
-    }
-
-    dispatch({
-      type: ACTIONS.SET_WEEK_BLOCKS_TOTAL,
-      payload: {
-        weekBlocksTotal: weekBlocksTotal,
-      },
-    });
-
-    let weekNodeData = { ...state.weekBlocksChart.props.children.props.data };
-    weekNodeData.datasets[0].data = blocksEarned;
-    weekNodeData.datasets[0].label = "LTO Blocks Earned";
-
-    dispatch({
-      type: ACTIONS.SET_WEEK_BLOCKS_CHART,
-      payload: {
-        weekBlocksChart: (
-          <div className="chart">
-            <Bar
-              data={weekNodeData}
-              height={300}
-              width={500}
-              options={{
-                title: {
-                  display: true,
-                  text: `LTO Blocks Earned Week of: ${
-                    weekStart.getMonth() + 1
-                  }/${weekStart.getDate()}`,
-                  position: "bottom",
-                  fontColor: "black",
-                  fontSize: 30,
-                },
-                legendPositon: "bottom",
-                maintainAspectRatio: false,
-                scales: {
-                  yAxes: [
-                    {
-                      ticks: {
-                        beginAtZero: true,
-                      },
-                    },
-                  ],
-                },
-              }}
-            />
-          </div>
-        ),
-      },
-    });
-
-    dispatch({
-      type: ACTIONS.SET_BAR_DATA_BOOL,
-      payload: { barDataBool: false },
-    });
-  }
 }
 
 const blocksChart = (
