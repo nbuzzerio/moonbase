3cb75086cde6b7a52a6f7ab27c0f5afee0c26044
Nicholas Buzzerio <nick.buzzerio@hotmail.com>
2021-04-13 19:58:43 -0400

    completed tablet section transitions

--- client/src/sections/LTOPrice.jsx
+++ client/src/sections/LTOPrice.jsx
@@ -6,8 +6,8 @@ export default function LTOPrice() {
       <svg className='eclipse'>
         <defs>
           <radialGradient id='radialGrad' x1='0%' y1='0%' x2='100%' y2='100%'>
-            <stop offset='75%' stopColor='#fff' stopOpacity='1.0'/>
-            <stop offset='100%' stopColor='#000' stopOpacity='.6'/>
+            <stop offset='70%' stopColor='#fff' stopOpacity='1.0'/>
+            <stop offset='100%' stopColor='#000' stopOpacity='.3'/>
           </radialGradient> 
         </defs>
         <circle r='50%' cx='50%' cy='50%' fill='url(#radialGrad)'/>
